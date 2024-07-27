e42669896960b9f7da1fcf7917559205d9609865
Alina Lytovchenko <alina.lytovchenko20@gmail.com>
2024-07-27 14:01:29 +0200

    Implement inability to count calories handling

--- src/service/messages-service.js
+++ src/service/messages-service.js
@@ -1,15 +1,13 @@
 const messages = require('../../messages.json'); 
 
 const getParsedReply = (chatgptReply) => {
-    console.log('chatgptReply')
-    console.log(chatgptReply)
     const parsedReply = JSON.parse(chatgptReply);
-    console.log('parsedReply')
-    console.log(parsedReply)
     if (!parsedReply.isFood) {
         return messages.foodRecognizingError;
     } else if (parsedReply.caloriesAmount) {
         return `The food in the picture is estimated to contain ${parsedReply.caloriesAmount} calories.`
+    } else if (parsedReply.isFood && !parsedReply.caloriesAmount) {
+        return messages.missunderstandingMsg;
     }
     return null;
 }
